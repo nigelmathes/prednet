683291a8ede88ea979737b63bb097a0f24c8e39d
Nigel Mathes <nmathes@morse-corp.com>
2017-10-18 11:44:31 -0400

    Cleaned up code, verified tensforflow runs (slowly) on CPU.

--- prednet_evaluate.py
+++ prednet_evaluate.py
@@ -1,31 +1,29 @@
-'''
-Evaluate trained PredNet on KITTI sequences.
+"""
+Evaluate trained PredNet on sequences in DATA_DIR.
 Calculates mean-squared error and plots predictions.
-'''
+"""
 
 import os
 import numpy as np
-from six.moves import cPickle
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-from keras import backend as K
 from keras.models import Model, model_from_json
-from keras.layers import Input, Dense, Flatten
+from keras.layers import Input
 
 from prednet import PredNet
 from data_utils import SequenceGenerator
-from kitti_settings import *
+from settings import *
 
+matplotlib.use('Agg')
 
 n_plot = 40
 batch_size = 10
 nt = 10
 
-weights_file = os.path.join(WEIGHTS_DIR, 'prednet_kitti_weights.hdf5')
-json_file = os.path.join(WEIGHTS_DIR, 'prednet_kitti_model.json')
+weights_file = os.path.join(WEIGHTS_DIR, 'prednet_weights.hdf5')
+json_file = os.path.join(WEIGHTS_DIR, 'prednet_model.json')
 test_file = os.path.join(DATA_DIR, 'X_test.hkl')
 test_sources = os.path.join(DATA_DIR, 'sources_test.hkl')
 
@@ -33,7 +31,7 @@ test_sources = os.path.join(DATA_DIR, 'sources_test.hkl')
 f = open(json_file, 'r')
 json_string = f.read()
 f.close()
-train_model = model_from_json(json_string, custom_objects = {'PredNet': PredNet})
+train_model = model_from_json(json_string, custom_objects={'PredNet': PredNet})
 train_model.load_weights(weights_file)
 
 # Create testing model (to output predictions)
@@ -55,9 +53,10 @@ if data_format == 'channels_first':
     X_hat = np.transpose(X_hat, (0, 1, 3, 4, 2))
 
 # Compare MSE of PredNet predictions vs. using last frame.  Write results to prediction_scores.txt
-mse_model = np.mean( (X_test[:, 1:] - X_hat[:, 1:])**2 )  # look at all timesteps except the first
-mse_prev = np.mean( (X_test[:, :-1] - X_test[:, 1:])**2 )
-if not os.path.exists(RESULTS_SAVE_DIR): os.mkdir(RESULTS_SAVE_DIR)
+mse_model = np.mean((X_test[:, 1:] - X_hat[:, 1:])**2)  # look at all timesteps except the first
+mse_prev = np.mean((X_test[:, :-1] - X_test[:, 1:])**2)
+if not os.path.exists(RESULTS_SAVE_DIR):
+    os.mkdir(RESULTS_SAVE_DIR)
 f = open(RESULTS_SAVE_DIR + 'prediction_scores.txt', 'w')
 f.write("Model MSE: %f\n" % mse_model)
 f.write("Previous Frame MSE: %f" % mse_prev)
@@ -65,23 +64,28 @@ f.close()
 
 # Plot some predictions
 aspect_ratio = float(X_hat.shape[2]) / X_hat.shape[3]
-plt.figure(figsize = (nt, 2*aspect_ratio))
+plt.figure(figsize=(nt, 2*aspect_ratio))
 gs = gridspec.GridSpec(2, nt)
 gs.update(wspace=0., hspace=0.)
 plot_save_dir = os.path.join(RESULTS_SAVE_DIR, 'prediction_plots/')
-if not os.path.exists(plot_save_dir): os.mkdir(plot_save_dir)
+if not os.path.exists(plot_save_dir):
+    os.mkdir(plot_save_dir)
 plot_idx = np.random.permutation(X_test.shape[0])[:n_plot]
 for i in plot_idx:
     for t in range(nt):
         plt.subplot(gs[t])
-        plt.imshow(X_test[i,t], interpolation='none')
-        plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
-        if t==0: plt.ylabel('Actual', fontsize=10)
+        plt.imshow(X_test[i, t], interpolation='none')
+        plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off',
+                        labelbottom='off', labelleft='off')
+        if t == 0:
+            plt.ylabel('Actual', fontsize=10)
 
         plt.subplot(gs[t + nt])
-        plt.imshow(X_hat[i,t], interpolation='none')
-        plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off', labelbottom='off', labelleft='off')
-        if t==0: plt.ylabel('Predicted', fontsize=10)
+        plt.imshow(X_hat[i, t], interpolation='none')
+        plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', right='off',
+                        labelbottom='off', labelleft='off')
+        if t == 0:
+            plt.ylabel('Predicted', fontsize=10)
 
-    plt.savefig(plot_save_dir +  'plot_' + str(i) + '.png')
+    plt.savefig(plot_save_dir + 'plot_' + str(i) + '.png')
     plt.clf()
--- prednet_train.py
+++ prednet_train.py
@@ -4,15 +4,12 @@ Train PredNet on DATA_DIR data
 
 import os
 import numpy as np
-from six.moves import cPickle
 
 from keras import backend as K
 from keras.models import Model
 from keras.layers import Input, Dense, Flatten
-from keras.layers import LSTM
 from keras.layers import TimeDistributed
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint
-from keras.optimizers import Adam
 
 from prednet import PredNet
 from data_utils import SequenceGenerator
@@ -38,16 +35,19 @@ N_seq_val = 100  # number of sequences to use for validation
 
 # Model parameters
 n_channels, im_height, im_width = (3, 128, 160)
-input_shape = (n_channels, im_height, im_width) if K.image_data_format() == 'channels_first' else (im_height, im_width, n_channels)
+input_shape = (n_channels, im_height, im_width) if K.image_data_format() == 'channels_first' else (im_height,
+                                                                                                   im_width,
+                                                                                                   n_channels)
 stack_sizes = (n_channels, 48, 96, 192)
 R_stack_sizes = stack_sizes
 A_filt_sizes = (3, 3, 3)
 Ahat_filt_sizes = (3, 3, 3, 3)
 R_filt_sizes = (3, 3, 3, 3)
-layer_loss_weights = np.array([1., 0., 0., 0.])  # weighting for each layer in final loss; "L_0" model:  [1, 0, 0, 0], "L_all": [1, 0.1, 0.1, 0.1]
+# weighting for each layer in final loss; "L_0" model:  [1, 0, 0, 0], "L_all": [1, 0.1, 0.1, 0.1]
+layer_loss_weights = np.array([1., 0., 0., 0.])
 layer_loss_weights = np.expand_dims(layer_loss_weights, 1)
 nt = 10  # number of timesteps used for sequences in training
-time_loss_weights = 1./ (nt - 1) * np.ones((nt,1))  # equally weight all timesteps except the first
+time_loss_weights = 1. / (nt - 1) * np.ones((nt, 1))  # equally weight all timesteps except the first
 time_loss_weights[0] = 0
 
 
@@ -57,9 +57,10 @@ prednet = PredNet(stack_sizes, R_stack_sizes,
 
 inputs = Input(shape=(nt,) + input_shape)
 errors = prednet(inputs)  # errors will be (batch_size, nt, nb_layers)
-errors_by_time = TimeDistributed(Dense(1, weights=[layer_loss_weights, np.zeros(1)], trainable=False), trainable=False)(errors)  # calculate weighted error by layer
+errors_by_time = TimeDistributed(Dense(1, weights=[layer_loss_weights, np.zeros(1)], trainable=False),
+                                 trainable=False)(errors)  # calculate weighted error by layer
 errors_by_time = Flatten()(errors_by_time)  # will be (batch_size, nt)
-final_errors = Dense(1, weights=[time_loss_weights, np.zeros(1)], trainable=False)(errors_by_time)  # weight errors by time
+final_errors = Dense(1, weights=[time_loss_weights, np.zeros(1)], trainable=False)(errors_by_time)
 model = Model(inputs=inputs, outputs=final_errors)
 model.compile(loss='mean_absolute_error', optimizer='adam')
 
@@ -70,11 +71,12 @@ val_generator = SequenceGenerator(val_file, val_sources, nt, batch_size=batch_si
 lr_schedule = lambda epoch: 0.001 if epoch < 75 else 0.0001
 callbacks = [LearningRateScheduler(lr_schedule)]
 if save_model:
-    if not os.path.exists(WEIGHTS_DIR): os.mkdir(WEIGHTS_DIR)
+    if not os.path.exists(WEIGHTS_DIR):
+        os.mkdir(WEIGHTS_DIR)
     callbacks.append(ModelCheckpoint(filepath=weights_file, monitor='val_loss', save_best_only=True))
 
 history = model.fit_generator(train_generator, samples_per_epoch / batch_size, nb_epoch, callbacks=callbacks,
-                validation_data=val_generator, validation_steps=N_seq_val / batch_size)
+                              validation_data=val_generator, validation_steps=N_seq_val / batch_size)
 
 if save_model:
     json_string = model.to_json()
